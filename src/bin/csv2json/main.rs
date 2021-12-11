e97ffb43c7d12a245fe94df16ef0cc5447ac9343
kaznak <kaznak.at.work@gmail.com>
2021-12-11 19:23:05 +0900

    src/bin/csv2json/main.rs: update

--- src/bin/csv2json/main.rs
+++ src/bin/csv2json/main.rs
@@ -7,26 +7,27 @@ use std::fs::File;
 use std::io;
 use std::process;
 
+fn do_work<R: io::Read>(mut rdr: csv::Reader<R>) -> Result<(), Box<dyn Error>> {
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
 
 fn run() -> Result<(), Box<dyn Error>> {
     let mut reader_builder = csv::ReaderBuilder::new();
     reader_builder.has_headers(false);
-    let path_result = env::args().nth(1);
-    if None == path_result {
-        let mut rdr = reader_builder.from_reader(io::stdin());
-        for result in rdr.records() {
-            let record = result?;
-            println!("{:?}", record);
+    match env::args().nth(1) {
+        None => {
+            let rdr = reader_builder.from_reader(io::stdin());
+            return do_work(rdr);
         }
-    } else {
-        let file_path = path_result.unwrap();
-        let mut rdr = reader_builder.from_reader(File::open(file_path)?);
-        for result in rdr.records() {
-            let record = result?;
-            println!("{:?}", record);
+        Some(file_path) => {
+            let rdr = reader_builder.from_reader(File::open(file_path)?);
+            return do_work(rdr);
         }
-    }
-    Ok(())
+    };
 }
 
 fn main() {
