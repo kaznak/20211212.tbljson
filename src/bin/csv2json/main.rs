2ed4f318be515e61767f8a2435f96ecf40cac13c
kaznak <kaznak.at.work@gmail.com>
2021-12-11 20:22:37 +0900

    src/bin/csv2json/main.rs: update

--- src/bin/csv2json/main.rs
+++ src/bin/csv2json/main.rs
@@ -16,12 +16,13 @@ fn do_work<R: io::Read>(mut rdr: csv::Reader<R>) -> Result<(), Box<dyn Error>> {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
+    let reader_raw: Box<dyn io::Read> = match env::args().nth(1) {
+        None => Box::new(io::stdin()),
+        Some(file_path) => Box::new(File::open(file_path)?),
+    };
     let mut reader_builder = csv::ReaderBuilder::new();
-    reader_builder.has_headers(false);
-    match env::args().nth(1) {
-        None => do_work(reader_builder.from_reader(io::stdin())),
-        Some(file_path) => do_work(reader_builder.from_reader(File::open(file_path)?)),
-    }
+    let rdr = reader_builder.has_headers(false).from_reader(reader_raw);
+    do_work(rdr)
 }
 
 fn main() {
