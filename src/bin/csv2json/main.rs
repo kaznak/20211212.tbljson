4c78386249eaae2cf2a56716b6941553bb43dfa6
kaznak <kaznak.at.work@gmail.com>
2021-12-11 11:47:36 +0900

    src/bin/csv2json/main.rs: use csv crate

--- src/bin/csv2json/main.rs
+++ src/bin/csv2json/main.rs
@@ -1,3 +1,11 @@
+extern crate csv;
+
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, csv2json!");
+    let mut rdr = csv::Reader::from_reader(stdin());
+    for result in rdr.records() {
+        let record = result.expect("a CSV record");
+        println!("{:?}", record);
+    }
 }
