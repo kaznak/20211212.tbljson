f1ea150a551b396ca20eb359a7c9fc35325f9c80
kaznak <kaznak.at.work@gmail.com>
2021-12-11 11:55:04 +0900

    src/bin/csv2json/main.rs: handle error

--- src/bin/csv2json/main.rs
+++ src/bin/csv2json/main.rs
@@ -1,11 +1,17 @@
 extern crate csv;
 
 use std::io::stdin;
+use std::process;
 
 fn main() {
     let mut rdr = csv::Reader::from_reader(stdin());
     for result in rdr.records() {
-        let record = result.expect("a CSV record");
-        println!("{:?}", record);
+        match result {
+            Ok(record) => println!("{:?}", record),
+            Err(err) => {
+                println!("error reading CSV from <stdin>: {}", err);
+                process::exit(1);
+            }
+        }
     }
 }
